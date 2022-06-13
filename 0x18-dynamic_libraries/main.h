3a1ceb9d0f356123914388bef00e103829a46575
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-06-13 21:18:22 +0300

    Update main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,19 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
-int _isupper(int c);
 int _isdigit(int c);
+int _isupper(int c;
 int _strlen(char *s);
-void _puts(char *s);
-char *_strcpy(char *dest, char *src);
+void _puts(char *str);
 int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -24,10 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int mod(int a, int b);
-extern div_t div(int __numer, int __denom);
-
-#endif /* MAIN_H */
+#endif
