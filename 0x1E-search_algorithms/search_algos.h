9caeb4e99a6b6dde729253035aa072315a356d01
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2022-10-04 14:17:37 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,35 +1,58 @@
-#ifndef SEARCH_ALGOS
-#define SEARCH_ALGOS
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* Library inclusions */
+#include <stddef.h>
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
+#include <math.h>
 
-/* helpers */
-void print_array(int *array, size_t inc, size_t size);
+/* Struct definitions */
 
-typedef struct skiplist_s
-{
-				int n;
-				size_t index;
-				struct skiplist_s *next;
-				struct skiplist_s *express;
-} skiplist_t;
-
+/**
+  * struct listint_s - singly linked list
+  *
+  * @n: Integer
+  * @index: Index of the node in a list
+  * @next: Pointer to the next node.
+  *
+  * Description: singly linked node structure for project.
+  */
 typedef struct listint_s
 {
-		int n;
-		size_t index;
-		struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
-/* main functions */
+
+/**
+  * struct skiplist_s - Singly linked list with express lane.
+  *
+  * @n: Integer.
+  * @index: Index of node in list.
+  * @next: Pointer to the next node.
+  * @express: Pointer to the next node in the express lane.
+  *
+  * Description: singly linked node structure with an
+  * express lane for search algorithms project.
+  */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+/* Function Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
-
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
-#endif
+
+#endif /*SEARCH_ALGOS_H */
